f9aa2fe2722de5f0650d8b751956ffbc74bcfe17
rh0mbus <wlegault_@hotmail.com>
2018-07-07 16:11:07 -0400

    Python solution for part 1

--- day2/day2p1.py
+++ day2/day2p1.py
@@ -1,1 +1,24 @@
 file = open('input.txt', 'r+', encoding='utf-8')
+
+lines = list()
+line_array = list()
+
+for line in file:
+    lines.append(line.rstrip())
+
+for line in lines:
+    line_array.append(line.split('\t'))
+
+file.close()
+
+def process(line):
+    temp = list()
+    for num in line:
+        temp.append(int(num))
+    return max(temp) - min(temp)
+
+total = 0
+for line in line_array:
+    total += process(line)
+
+print(f'Checksum: {total}')
