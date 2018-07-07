a49c38d50d33c08fba7b22e7342d539957be0231
rh0mbus <wlegault_@hotmail.com>
2018-07-06 21:02:37 -0400

    Python solution for day1 puzzle 1

---
+++ day1/puzzle1/day1p1.py
@@ -0,0 +1,29 @@
+file = open('input.txt', ('r+'), encoding=('utf-8'))
+
+lines = list()
+numArr = list()
+
+for line in file:
+    lines.append(line)
+
+# Convert to list of individual ints
+for num in line:
+    numArr.append(int(num))
+
+# Index and accumulator
+counter = 0
+total = 0
+
+# Process array
+for num in numArr:
+    if counter < len(numArr) - 1:
+        if num == numArr[counter + 1]:
+            total += num
+    counter += 1
+
+# Check for circular case
+if numArr[-1] == numArr[0]:
+    total += numArr[-1]
+
+print(total)
+file.close()
